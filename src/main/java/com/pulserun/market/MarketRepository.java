8681c450a8bdb67687ddc40b6ab8f94ab566a44d
doer <doer1195@gmail.com>
2026-03-20 01:58:58 +0900

    feat: market 레포지토리 추가

---
+++ src/main/java/com/pulserun/market/MarketRepository.java
@@ -0,0 +1,15 @@
+package com.pulserun.market;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MarketRepository extends JpaRepository<Market,Long> {
+
+    Optional<Market> findByName(String name);
+
+    boolean existsByCode(String code);
+}
