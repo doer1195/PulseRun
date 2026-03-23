9360faa5e0d91303316aa0f9b2c2d72577026320
doer <doer1195@gmail.com>
2026-03-24 00:49:15 +0900

    feat: UserRepository 불필요한 메서드 제거

--- src/main/java/com/pulserun/user/repository/UserRepository.java
+++ src/main/java/com/pulserun/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
-package com.pulserun.user;
+package com.pulserun.user.repository;
 
+import com.pulserun.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,13 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
-
     Optional<User> findByProviderAndProviderId(String Provider, String ProviderId);
-
-    boolean existsByEmail(String email);
-
-    boolean existsByProviderAndProviderId(String Provider, String ProviderId);
-
-    boolean existsByNickname(String nickname);
 }
