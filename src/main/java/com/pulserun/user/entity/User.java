353a96b5d74f50ee30a30864407a2f16388a4d2b
doer <doer1195@gmail.com>
2026-03-24 00:53:31 +0900

    feat: 유저 생성자 null 검증 로직 추가

--- src/main/java/com/pulserun/user/entity/User.java
+++ src/main/java/com/pulserun/user/entity/User.java
@@ -1,5 +1,7 @@
-package com.pulserun.user;
+package com.pulserun.user.entity;
 
+import com.pulserun.global.error.ErrorCode;
+import com.pulserun.global.utils.Asserts;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -11,20 +13,28 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "provider", nullable = false)
     private String provider;
+
     @Column(name = "provider_id", nullable = false)
     private String providerId;
+
     @Column(name = "email")
     private String email;
+
     @Column(name = "nickname")
     private String nickname;
 
     @Builder
     public User(Long id, String provider, String providerId, String email, String nickname) {
+        Asserts.notNull(provider, ErrorCode.DATA_IS_NULL, "provider");
+        Asserts.notNull(providerId, ErrorCode.DATA_IS_NULL, "providerId");
+
         this.id = id;
         this.provider = provider;
         this.providerId = providerId;
