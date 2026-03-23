74a611d2c275ad36cc0893397bbc19e8c292b44b
doer <doer1195@gmail.com>
2026-03-24 00:51:20 +0900

    feat: 예외명 변경 및 에러코드 세분화

--- src/main/java/com/pulserun/wishlist/WishlistService.java
+++ src/main/java/com/pulserun/wishlist/WishlistService.java
@@ -1,9 +1,11 @@
 package com.pulserun.wishlist;
 
-import com.pulserun.market.Market;
-import com.pulserun.market.MarketRepository;
-import com.pulserun.user.User;
-import com.pulserun.user.UserRepository;
+import com.pulserun.global.error.ErrorCode;
+import com.pulserun.global.error.exception.PulserunException;
+import com.pulserun.market.entity.Market;
+import com.pulserun.market.repository.MarketRepository;
+import com.pulserun.user.entity.User;
+import com.pulserun.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,21 +21,28 @@ public class WishlistService {
     private final UserRepository userRepository;
     private final MarketRepository marketRepository;
 
+    public List<Wishlist> getMyWishlist(Long userId) {
+        User user = userRepository.findById(userId)
+                                  .orElseThrow(() -> new PulserunException(ErrorCode.USER_NOT_FOUND));
+
+        return wishlistRepository.findAllByUser(user);
+    }
+
     @Transactional
-    public void addWish(Long providerId, Long marketId) {
-        User user = userRepository.findById(providerId)
-                .orElseThrow(() -> new IllegalArgumentException("user does not exist"));
+    public void addWish(Long userId, Long marketId) {
+        User user = userRepository.findById(userId)
+                                  .orElseThrow(() -> new PulserunException(ErrorCode.USER_NOT_FOUND));
         Market market = marketRepository.findById(marketId)
-                .orElseThrow(() -> new IllegalArgumentException("market does not exist"));
+                                        .orElseThrow(() -> new PulserunException(ErrorCode.MARKET_NOT_FOUND));
 
         if (wishlistRepository.existsByUserAndMarket(user, market)) {
-            throw new IllegalStateException("already added");
+            throw new PulserunException(ErrorCode.WISHLIST_ALREADY_EXIST);
         }
 
         Wishlist wishlist = Wishlist.builder()
-                .user(user)
-                .market(market)
-                .build();
+                                    .user(user)
+                                    .market(market)
+                                    .build();
 
         wishlistRepository.save(wishlist);
     }
@@ -41,20 +50,13 @@ public class WishlistService {
     @Transactional
     public void removeWish(Long userId, Long marketId) {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("user does not exist"));
+                                  .orElseThrow(() -> new PulserunException(ErrorCode.USER_NOT_FOUND));
         Market market = marketRepository.findById(marketId)
-                .orElseThrow(() -> new IllegalArgumentException("market does not exist"));
+                                        .orElseThrow(() -> new PulserunException(ErrorCode.MARKET_NOT_FOUND));
 
         Wishlist wishlist = wishlistRepository.findByUserAndMarket(user, market)
-                .orElseThrow(() -> new IllegalArgumentException("already removed"));
+                                              .orElseThrow(()-> new PulserunException(ErrorCode.WISHLIST_NOT_FOUND));
 
         wishlistRepository.delete(wishlist);
-    }
-
-    public List<Wishlist> getMyWishlist(Long userId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("user does not exist"));
-
-        return wishlistRepository.findAllByUser(user);
     }
 }
