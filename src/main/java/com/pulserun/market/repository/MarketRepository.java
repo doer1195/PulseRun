aa135dba982583075d9286bd2763f8f02fb89b2c
doer <doer1195@gmail.com>
2026-03-24 00:44:03 +0900

    refactor: 불필요한 메서드 제거

--- src/main/java/com/pulserun/market/repository/MarketRepository.java
+++ src/main/java/com/pulserun/market/repository/MarketRepository.java
@@ -1,14 +1,11 @@
-package com.pulserun.market;
+package com.pulserun.market.repository;
 
+import com.pulserun.market.entity.Market;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface MarketRepository extends JpaRepository<Market, Long> {
-
-    Optional<Market> findByName(String name);
 
     boolean existsByCode(String code);
 }
