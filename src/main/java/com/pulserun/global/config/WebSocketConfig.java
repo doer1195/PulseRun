7ee6cf406b5efb28d067539a80de4e4c872d03e0
doer <doer1195@gmail.com>
2026-03-24 00:49:53 +0900

    refactor: WebSocketConfig 글로벌 패키지로 이동

--- src/main/java/com/pulserun/global/config/WebSocketConfig.java
+++ src/main/java/com/pulserun/global/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.pulserun.market;
+package com.pulserun.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
