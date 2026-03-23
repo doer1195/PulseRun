0cc7a6a8434d9accf16702da2bbfc898630b48bb
doer <doer1195@gmail.com>
2026-03-24 00:43:44 +0900

    feat: null 체크 검증 로직 추가

--- src/main/java/com/pulserun/market/entity/Market.java
+++ src/main/java/com/pulserun/market/entity/Market.java
@@ -1,5 +1,7 @@
-package com.pulserun.market;
+package com.pulserun.market.entity;
 
+import com.pulserun.global.error.ErrorCode;
+import com.pulserun.global.utils.Asserts;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -11,6 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "markets")
 public class Market {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,6 +26,8 @@ public class Market {
 
     @Builder
     public Market(String code, String name) {
+        Asserts.notNull(code, ErrorCode.DATA_IS_NULL, "code");
+        Asserts.notNull(name, ErrorCode.DATA_IS_NULL, "name");
         this.code = code;
         this.name = name;
     }
